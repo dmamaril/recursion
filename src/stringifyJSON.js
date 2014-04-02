710c6c32aa8e0a587fd10062dddc786677fd3bf0
Don Mamaril <mamarildon@gmail.com>
2014-04-02 10:31:05 -0700

    Updated stringifyJSON after SpecRunner fix

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,47 +3,39 @@
 
 // but you don't so you're going to have to write it from scratch:
 
+
 function stringifyJSON (obj) {
-    var result;
-    if (typeof obj !== 'function' || typeof obj !== 'undefined') {
+    
+    if (obj === null) {
+      return 'null';
+    }
+	else if (typeof obj === 'string') {
+      return '"' + obj + '"';
+    }
 
-        if (typeof obj === 'null') {
-            result = 'null';
-        } 
+	else if (Array.isArray(obj)) {
+      var isArr = [];
+      _.each(obj, function(element, key) {
+		    isArr.push(stringifyJSON(element));
+      });
+      return '[' + isArr.toString() + ']';
+    }
 
+	else if (typeof obj === 'object') {
+      var isObj = [];
+      _.each(obj, function(element, key) {
+		if (typeof element !== 'function') {
+	    	if (typeof element !== 'undefined') {
+				isObj.push(stringifyJSON(key) + ':' + stringifyJSON(element));		
+			}
+		}
+      });
+      return '{' + isObj.toString() + '}';
+    }
 
-          else if (typeof obj === 'string') {
-            result = '"' + obj + '"';
-        } 
+    else {
+      return obj.toString();
+	}
 
-
-          else if (Array.isArray(obj)) {
-            var isArr = [];
-            _.each(obj, function(element) {
-                isArr.push(stringifyJSON(element));
-            });
-            result = '[' + isArr.toString() + ']';
-        }
-
-
-          else if (typeof obj === 'object') {
-            var isObj = [];
-            _.each(obj, function(element, key) {
-                isObj.push(stringifyJSON(key) + ':' + stringifyJSON(element));
-            });
-            console.log(isObj);
-            result =  '{' + isObj.toString() + '}';
-        }
-
-
-          else {
-            result = obj.toString();
-         }
-
-    };
-    return result;
 };
 
-// including missing variables for SpecRunner.html
-var arrayWithValidElements = [true,'false',42];
-var objectWithInvalidAttributes = {x: 5, y: 6};
